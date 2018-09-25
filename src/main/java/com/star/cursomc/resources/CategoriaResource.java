b315c7621cdc3341735a0d640a4a8cb45af1a16f
Léo Celso Rego Araújo <leocraraujo@gmail.com>
2018-09-25 14:29:22 -0300

    Anotaçao para o Put

--- src/main/java/com/star/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/star/cursomc/resources/CategoriaResource.java
@@ -37,6 +37,7 @@ public class CategoriaResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
         obj.setId(id);
         obj = service.update(obj);
