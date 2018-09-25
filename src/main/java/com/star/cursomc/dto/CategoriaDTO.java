1fa1b82427867c52fede1edaa9414a1d12140e8e
Léo Celso Rego Araújo <leocraraujo@gmail.com>
2018-09-25 16:55:58 -0300

    Listando categorias sem produtos com DTO

---
+++ src/main/java/com/star/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,42 @@
+package com.star.cursomc.dto;
+
+import com.star.cursomc.domain.Categoria;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(Categoria obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/com/star/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/star/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,16 @@
 package com.star.cursomc.resources;
 
 import com.star.cursomc.domain.Categoria;
+import com.star.cursomc.dto.CategoriaDTO;
 import com.star.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -24,6 +24,24 @@ public class CategoriaResource {
 
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+//    @RequestMapping(method = RequestMethod.GET)
+//    public ResponseEntity<List<Categoria>> findAll(){
+//
+//        List<Categoria> list = service.findAll(); //retorna todas as categorias
+//        return ResponseEntity.ok().body(list);
+//    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+
+        //retorna todas as categorias
+        List<Categoria> list = service.findAll();
+        //para retornar apenas as categorias sem os seus produtos convertemos a lista de categorias numa lista de categoriasDTO
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
     }
 
     @RequestMapping(method = RequestMethod.POST)
